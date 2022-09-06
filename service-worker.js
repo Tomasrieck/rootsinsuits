fe05469c72584c63d77fb9e4dc76f7000654065b
Tomasrieck <Tomasrieck@gmail.com>
2022-09-06 11:32:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45dc323b8ace10553ab4e2656a1fd769.js"
+  "/precache-manifest.c84b70c87656cad88a9f0d3064f901fe.js"
 );
 
 workbox.clientsClaim();
