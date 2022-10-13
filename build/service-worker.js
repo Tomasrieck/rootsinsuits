43a36d57fc9dafbd26b406eb0732661d15b63f7a
Tomasrieck <Tomasrieck@gmail.com>
2022-10-13 16:27:33 +0200

    ef

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b118f01699ec76368c63575dcca1bda.js"
+  "/rootsinsuits/precache-manifest.66e536a55e069ebf6e3d76ded267c939.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rootsinsuits/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
